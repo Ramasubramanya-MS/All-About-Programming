3388d074b15173ac2dded02c0b0d1b63e5dfade5
Ramasubramanya-MS <81099796+Ramasubramanya-MS@users.noreply.github.com>
2021-07-09 15:42:55 +0530

    Update M1.sql

--- SQL/M1.sql
+++ SQL/M1.sql
@@ -25,3 +25,58 @@ copy Customer_table from 'C:\Program Files\PostgreSQL\13\data\data_copy\copy.csv
 
 copy Customer_table from 'C:\Program Files\PostgreSQL\13\data\Data_copy\copytext.txt' delimiter ',' csv header;
 
+SELECT * FROM customer_table;
+
+SELECT first_name from customer_table;
+SELECT first_name, last_name from customer_table;
+SELECT * FROM customer_table;
+
+SELECT DISTINCT first_name FROM customer_table;
+SELECT DISTINCT first_name, age FROM customer_table;
+SELECT DISTINCT * FROM customer_table;
+
+SELECT first_name FROM customer_table WHERE age > 25;
+SELECT DISTINCT first_name FROM customer_table WHERE age > 25;
+
+SELECT first_name, last_name, age FROM customer_table WHERE age>20 and age<30;
+SELECT first_name, last_name, age from customer_table WHERE age<20 OR age>30;
+
+SELECT * FROM customer_table WHERE NOT age = 25;
+SELECT * FROM customer_table WHERE NOT age = 25 AND NOT first_name = 'Jay';
+
+UPDATE Customer_table
+SET last_name='Pe', age = 17 WHERE cust_id=6;
+
+SELECT * FROM customer_table;
+
+UPDATE Customer_table SET email_id = 'gee@xyz.com' WHERE first_name='Gee' or first_name = 'gee';
+SELECT * FROM customer_table;
+
+ALTER TABLE customer_table ADD tesT VARCHAR(255);
+SELECT * FROM customer_table;
+
+ALTER TABLE customer_table DROP test;
+SELECT * FROM customer_table;
+
+ALTER TABLE customer_table DROP COLUMN test;
+
+ALTER TABLE customer_table ALTER COLUMN age TYPE varchar(255);
+SELECT * FROM customer_table;
+
+ALTER TABLE customer_table RENAME COLUMN email_id TO customer_email;
+SELECT * FROM customer_table;
+
+ALTER TABLE customer_table ALTER COLUMN cust_id SET NOT null;
+SELECT * FROM customer_table;
+INSERT INTO customer_table(first_name,last_name,age,customer_email) VALUES
+('aa','bb','25','ab@xyz.com');
+
+ALTER TABLE customer_table ALTER COLUMN cust_id SET NOT null;
+
+ALTER TABLE customer_table ADD CONSTRAINT cust_id CHECK (cust_id>0);
+INSERT INTO customer_table VALUES (-1,'cc','dd','67','cd@xyz.com');
+
+SELECT * FROM customer_table;
+ALTER TABLE customer_table ADD PRIMARY KEY (cust_id);
+SELECT * FROM customer_table;
+
